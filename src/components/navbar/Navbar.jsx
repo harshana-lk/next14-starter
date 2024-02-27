61cf7f930fa6030b2cf7bde9d78661897b2c4cc4
harshana-lk <harshuonlive@gmail.com>
2024-02-28 00:45:45 +0530

    ♻️ Styled NavBar & added propper Navigation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import Links from "./links/Links";
-
+import styles from "./navbar.module.css";
+import Logo from "@/lib/logo.png";
+import Image from "next/image";
 const Navbar = () => {
   return (
-    <div>
-      <div>Logo</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <Image src={Logo} alt="logo" width={140} height="auto" />
+      </div>
       <div>
         <Links />
       </div>
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import styles from "./links.module.css";
+import NavLink from "./navLink/navLink";
 
 const Links = () => {
   const links = [
@@ -21,11 +22,9 @@ const Links = () => {
   ];
 
   return (
-    <div>
+    <div className={styles.links}>
       {links.map((link) => (
-        <Link href={link.path} key={link.name}>
-          {link.name}
-        </Link>
+        <NavLink item={link} key={link.name} />
       ))}
     </div>
   );
