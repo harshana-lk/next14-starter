a9285de42f5e046d997a320eb455fa49b68c11f7
harshana-lk <harshuonlive@gmail.com>
2024-02-27 14:03:30 +0530

    ➕ Added More Functions to the App

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>Page Not Found</h2>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+};
+export default NotFound;
